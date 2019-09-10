77d2f99e4b927d3e450a92d38d9d13e576411ef9
Kresimir@home <kreso18@gmail.com>
2019-09-10 12:06:35 +0200

    Boolean.IfTrue(Action action)

---
+++ Exstensions.Tests/BooleanExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Exstensions.Tests
+{
+    [TestClass]
+    public class BooleanExtensionsTests
+    {
+
+        [TestMethod]
+        public void IfTrue()
+        {
+            int value1 = 1;
+            int value2 = 2;
+
+            const bool @true = true;
+            const bool @false = false;
+
+            @true.IfTrue(() => value1 = 1000); // value1 = 1000;
+            @false.IfTrue(() => value2 = 2000); // value2 = 2;
+
+            Assert.AreEqual(1000, value1);
+            Assert.AreEqual(2, value2);
+        }
+
+       
+    }
+}
---
+++ Exstensions/BooleanExstensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exstensions
+{
+    public static class BooleanExstensions
+    {
+        /// <summary>
+        ///     A bool extension method that execute an Action if the value is true.
+        /// </summary>
+        public static void IfTrue(this bool value, Action action)
+        {
+            if (value)
+                action();
+        }
+
+    }
+}
